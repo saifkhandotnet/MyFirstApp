85a89841d7d5f9e281ed5da93ec27840a06420bd
saifkhandotnet <38806311+saifkhandotnet@users.noreply.github.com>
2018-04-28 14:29:40 +0530

    CSS changes

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles: ['P { color: blue}']
+})
+export class ServerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
